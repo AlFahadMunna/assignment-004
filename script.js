bb194500e74bd3686073ad5bc7f1d33db4b3e1aa
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-02-23 12:32:58 -0600

    toggle style button logic added

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let total = document.getElementById("total");
 let interviewCount = document.getElementById("interview-count");
 let rejectedCount = document.getElementById("rejected-count");
 
+const allFilterBtn = document.getElementById("all-btn");
+const interviewFilterBtn = document.getElementById("interview-btn");
+const rejectedFilterBtn = document.getElementById("rejected-btn");
+
 const allCardsSection = document.getElementById("cards");
 
 const mainContainer = document.querySelector("main");
@@ -15,3 +19,17 @@ function calculateCount() {
   rejectedCount.innerText = rejectedList.length;
 }
 calculateCount();
+
+function toggleStyle(id) {
+  allFilterBtn.classList.remove("bg-blue-500", "text-white");
+  interviewFilterBtn.classList.remove("bg-white", "text-white");
+  rejectedFilterBtn.classList.remove("bg-white", "text-white");
+
+  allFilterBtn.classList.add("bg-white", "text-black");
+  interviewFilterBtn.classList.add("bg-white", "text-black");
+  rejectedFilterBtn.classList.add("bg-white", "text-black");
+
+  const selected = document.getElementById(id);
+  selected.classList.remove("bg-white", "text-black");
+  selected.classList.add("bg-blue-500", "text-white");
+}
