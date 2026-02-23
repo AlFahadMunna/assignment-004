2bfd0f326eed41a732e5200abffb14111e9b7a05
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-02-23 12:07:01 -0600

    calculate count function added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("total");
+let interviewCount = document.getElementById("interview-count");
+let rejectedCount = document.getElementById("rejected-count");
+
+const allCardsSection = document.getElementById("cards");
+
+const mainContainer = document.querySelector("main");
+
+function calculateCount() {
+  total.innerText = allCardsSection.children.length;
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
+}
+calculateCount();
