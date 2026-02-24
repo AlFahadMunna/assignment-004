a3e20729ea64366b5d782c99e2638fc0b5db4f17
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-02-23 20:21:46 -0600

    some changes added for classname

--- script.js
+++ script.js
@@ -69,6 +69,42 @@ function renderedInterview() {
     console.log(interview);
     let div = document.createElement("div");
     div.className = "card flex justify-between mt-4 bg-white rounded-md p-6";
-    div.innerHTML = ``;
+    div.innerHTML = `
+    
+    <div class="card flex justify-between mt-4 bg-white rounded-md p-6">
+          <!-- left part -->
+          <div>
+            <h1 class="font-bold job-name">Mobile First Corp</h1>
+            <h4 class="job-title text-[#64748b] font-light">
+              React Native Developer
+            </h4>
+            <p class="job-des text-[#64748b] font-light mt-5">
+              Remote • Full-time • $130,000 - $175,000
+            </p>
+            <p
+              class="job-status status mt-5 px-4 w-[130px] text-center text-[14px] py-2 rounded-md uppercase bg-[#eef4ff]"
+            >
+              Not Applied
+            </p>
+            <p class="job-type text-[#64748b] font-light">
+              Build cross-platform mobile applications using React Native. Work
+              on products used by millions of users worldwide.
+            </p>
+            <div class="mt-5">
+              <button
+                class="interview-btn px-3 py-2 border uppercase rounded-md border-green-500"
+              >
+                interview
+              </button>
+              <button
+                class="rejected-btn px-3 py-2 border uppercase rounded-md ml-1 border-red-400"
+              >
+                Rejected
+              </button>
+            </div>
+          </div>
+          <!-- right part -->
+          <div><img src="delete.png" alt="" /></div>
+        </div>`;
   }
 }
